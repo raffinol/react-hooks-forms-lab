6fce0052949573ada94a193a4358347553ef7c36
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-04 17:55:05 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import Header from "./Header";
-import itemData from "../data/items";
+import React, { useState } from 'react';
+import ShoppingList from './ShoppingList';
+import Header from './Header';
+import itemData from '../data/items';
 
 function App() {
   const [items, setItems] = useState(itemData);
@@ -11,10 +11,14 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
+
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+    <div className={'App ' + (isDarkMode ? 'dark' : 'light')}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
